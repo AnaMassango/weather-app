f167bd7bc1f7dbe3ff08f4c0a6b085404acb1a7b
AnaMassango <143935476+AnaMassango@users.noreply.github.com>
2023-10-02 14:10:17 +0100

    update

--- src/app.js
+++ src/app.js
@@ -29,54 +29,6 @@ function formatDay(timestamp) {
   return days[day];
 }
 
-function displayForecast(response) {
-  let forecast = response.data.daily;
-
-  let forecastElement = document.querySelector("#forecast");
-  let forecastHTML = `<div class="row">`;
-  let days = ["Thu", "Fri", "Sat"];
-  forecast.forEach(function (forecastDay, index) {
-    //looping object days in parameter day
-    if (index < 6) {
-      forecastHTML =
-        forecastHTML +
-        `
-              <div class="col-2">
-                <div class="weather-forecast-date">${formatDay(
-                  forecastDay.dt
-                )}</div>
-               
-                <img
-                  src="http://openweathermap.org/img/wn/${
-                    forecastDay.weather[0].icon
-                  }@2x.png"
-                  alt=""
-                  width="42"
-                />
-                <div class="weather-forecast-temperatures">
-                  <span class="weather-forecast-temperature-max">${Math.round(
-                    forecastDay.temp.max
-                  )}° </span>
-                  <span class="weather-forecast-temperature-min">${Math.round(
-                    forecastDay.temp.min
-                  )}° </span>
-                </div>
-                </div>
-              `;
-    }
-  });
-
-  forecastHTML = forecastHTML + `</div>`;
-  forecastElement.innerHTML = forecastHTML;
-}
-function getForecast(coordinates) {
-  //getForecast is running in displayTemperature with response.data.coord
-  console.log(coordinates);
-  let apiKey = "0dc40d3d7cda209ca40e77430c74cf57";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
-  console.log(apiUrl);
-  axios.get(apiUrl).then(displayForecast);
-}
 
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
